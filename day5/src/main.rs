1a3bccfc9ae25910bdb3b36222f1fcdc1d75c027
Rythmyr <rythmyr@gmail.com>
2023-01-22 01:56:18 -0600

    day 5 basic i/o setup done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std:: path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("simple.txt") {
+        for line in lines {
+            
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    io::BufReader::new(file).lines().collect()
 }
